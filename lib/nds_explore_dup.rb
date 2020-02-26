5a829393f61f07e38e305d45ed992c0e6935d520
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-26 08:36:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore_dup.rb
+++ lib/nds_explore_dup.rb
@@ -5,11 +5,11 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
+  # Change the code below to pretty print the nds with pp
   pp nds
 end
 
 def print_first_directors_movie_titles
-  
   movies = directors_database[0][:movies]
 	
 	movie_index = 0
@@ -18,7 +18,4 @@ def print_first_directors_movie_titles
 		movie_index += 1
 	end
 end
-
-return print_first_directors_movie_titles
-
   
